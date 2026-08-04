1da01ca3ea663da3fab5c7c88c29d0d61472e819
varun <malthumkarvarun@gmail.com>
2026-08-04 15:09:07 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (58.47%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -9,37 +9,35 @@
  * }
  */
 class Solution {
-    ListNode addTwoNumbers(ListNode l1, ListNode l2) //l1 and l2 are 2 single linked lists 
+    ListNode addTwoNumbers(ListNode l1, ListNode l2)
     {
-        ListNode dummy = new ListNode(0); // Dummy node to build the answer list
+        ListNode dummy = new ListNode(0); 
 
-        ListNode curr = dummy; // Pointer used to add new nodes
+        ListNode curr = dummy; 
+        int carry = 0; 
 
-        int carry = 0; // Stores carry after addition
-
-        while (l1 != null || l2 != null || carry != 0)  // Continue while either list has nodes or carry is left
+        while (l1 != null || l2 != null || carry != 0) 
         {
         {
-            int sum = carry;  // Start with previous carry
-
-            if(l1 != null)  // If l1 has a node, add its value
+            int sum = carry;
+            if(l1 != null)  
             {
                 sum = sum + l1.val;
                 l1 = l1.next;
 
             }
 
-            if(l2 != null) // If l2 has a node, add its value
+            if(l2 != null) 
             {
                 sum = sum + l2.val;
                 l2 = l2.next;
             }
 
-            carry = sum / 10; // Store carry for the next addition
-            curr.next = new ListNode(sum % 10); // Create a node with the last digit of sum
-            curr = curr.next; // Move current pointer to the new node
+            carry = sum / 10; 
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
         } 
     }
-    return dummy.next; // Skip dummy node and return actual answer
+    return dummy.next;
 }
 }

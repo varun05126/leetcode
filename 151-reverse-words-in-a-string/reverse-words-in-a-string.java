ce59245afb0623567ce24f7c715a19fbb11a144b
varun <malthumkarvarun@gmail.com>
2026-08-05 10:33:30 +0530

    Time: 7 ms (63.40%) | Memory: 44.4 MB (46.77%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -6,14 +6,9 @@ class Solution {
 
         for(int i = words.length -1; i>=0; i--)
         {
-            ans.append(words[i]);
-
-            if(i!=0)
-            {
-                ans.append(" ");
-            }
+            ans.append(words[i]).append(" ");
         }
 
-        return ans.toString();
+        return ans.toString().trim();
     }
 }
